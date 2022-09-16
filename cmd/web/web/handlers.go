d53cb7cdfcbfe4ad44fd7cfe8679f38e5015ea62
Chris <atnl@protonmail.com>
2022-09-16 18:25:13 -0400

    Delete cmd/web/web directory

--- cmd/web/web/handlers.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "net/http"
-
-func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
-		app.errorLog.Println(err)
-	}
-}
