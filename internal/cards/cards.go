37c5123d6d770cef8961307dad1eaad8159d9fc8
Chris <atnl@protonmail.com>
2022-10-26 11:31:21 -0400

    Update cards.go

--- internal/cards/cards.go
+++ internal/cards/cards.go
@@ -1,91 +1,72 @@
-package main
+package cards
 
 import (
-	"encoding/json"
-	"myapp/internal/cards"
-	"net/http"
-	"strconv"
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
 )
 
-type stripePayload struct {
-	Currency string `json:"currency"`
-	Amount   string `json:"amount"`
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
 }
 
-type jsonResponse struct {
-	OK      bool   `json:"ok"`
-	Message string `json:"message,omitempty"`
-	Content string `json:"content,omitempty"`
-	ID      int    `json:"id,omitempty"`
+type Transaction struct {
+	TransactionStatusID int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BankReturnCode      string
 }
 
-// GetPaymentIntent This request will be called using POST, and contain the request body in json format.
-// That json file, will conform to the standard set in stripePayload.
-func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
-	var payload stripePayload
+// alias to payment intent.
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.CreatePaymentIntent(currency, amount)
+}
 
-	// Decode body of that request into that variable payload.
-	err := json.NewDecoder(r.Body).Decode(&payload)
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+
+	// create a payment intent
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	//params.AddMetadata("key", "value")
+	pi, err := paymentintent.New(params)
 	if err != nil {
-		app.errorLog.Println(err)
-		return
-	}
-
-	// Convert the payload data to Alphanumeric format.
-	amount, err := strconv.Atoi(payload.Amount)
-	if err != nil {
-		app.errorLog.Println(err)
-		return
-	}
-
-	// Gain access to needed methods
-	// The values for Secret and Key will be pulled from app.config.
-	card := cards.Card{
-		Secret:   app.config.stripe.secret,
-		Key:      app.config.stripe.key,
-		Currency: payload.Currency,
-	}
-
-	/*
-	   Declare a variable, name it OK, with the assumption that it will be true
-	   Then, call the function charge in cards and get the three variables that are returns.
-
-	   Payment Intent (pi)
-	   message (msg)
-	   error (err)
-
-	   Populate them by calling card.Charge() and requires 2 vars the currency and the amount.
-	*/
-	okay := true
-	pi, msg, err := card.Charge(payload.Currency, amount)
-	if err != nil {
-		okay = false
-	}
-
-	if okay {
-		out, err := json.MarshalIndent(pi, "", "   ") // Marshal returns the JSON encoding.
-		if err != nil {                               
-			app.errorLog.Println(err)
-			return
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeErr.Code)
 		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(out)
-
-		// But if things don't work..
-	} else {
-		j := jsonResponse{
-			OK:      false,
-			Message: msg,
-			Content: "",
-		}
-
-		out, err := json.MarshalIndent(j, "", "   ")
-		if err != nil {
-			app.errorLog.Println(err)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(out)
+		return nil, msg, err
 	}
+	return pi, "", nil
+}
+
+// cardErrorMessage returns human readable versions of card error messages
+func cardErrorMessage(code stripe.ErrorCode) string {
+	var msg = ""
+	switch code {
+	case stripe.ErrorCodeCardDeclined:
+		msg = "Your card was declined"
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your card is expired"
+	case stripe.ErrorCodeIncorrectCVC:
+		msg = "Incorrect CVC code"
+	case stripe.ErrorCodeIncorrectZip:
+		msg = "Incorrect zip/postal code"
+	case stripe.ErrorCodeAmountTooLarge:
+		msg = "The amount is too large to charge to your card"
+	case stripe.ErrorCodeAmountTooSmall:
+		msg = "The amount is too small to charge to your card"
+	case stripe.ErrorCodeBalanceInsufficient:
+		msg = "Insufficient balance"
+	case stripe.ErrorCodePostalCodeInvalid:
+		msg = "Your postal code is invalid"
+	default:
+		msg = "Your card was declined"
+	}
+	return msg
 }
