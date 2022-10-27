167fb01d0efa7f1a47f28c44e79469808720b63d
Chris <atnl@protonmail.com>
2022-10-26 22:19:19 -0400

    Update handlers-api.go

--- cmd/web/api/handlers-api.go
+++ cmd/web/api/handlers-api.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 )
 
+// Used in the GetPaymentIntent function below.
 type stripePayload struct {
 	Currency string `json:"currency"`
 	Amount   string `json:"amount"`
@@ -22,24 +23,34 @@ type jsonResponse struct {
 // GetPaymentIntent This request will be called using POST, and contain the request body in json format.
 // That json file, will conform to the standard set in stripePayload.
 func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
+	// Declaring the payload variable as type stripePayload. (from the struct above)
 	var payload stripePayload
 
-	// Decode body of that request into that variable payload.
+	// Retrieve the body of the request and decode it into the variable payload.
+	// The payload variable will be populated with the currency and amount.
+	// err is assigned the value of: from the JSON pkg, get a new decoder,
+	// and decode the request body into a reference to payload.
 	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
 		app.errorLog.Println(err)
 		return
 	}
 
-	// Convert the payload data to Alphanumeric format.
+	// The currency is a string and the amount is a string.
+	// Need them to be a numeric value. So use Atoi: Alpha to Numeric.
+	// Decode body of that request into that variable payload
 	amount, err := strconv.Atoi(payload.Amount)
 	if err != nil {
 		app.errorLog.Println(err)
 		return
 	}
 
-	// Gain access to needed methods
-	//The values for Secret and Key will be pulled from app.config.
+	/*
+		Utilize the cards dot card type:
+		Create a variable, named card, and use the cards package to assign it's value.
+		And populate it's fields. Gain access to needed methods
+		The values for Secret and Key will be pulled from app.config.
+	*/
 	card := cards.Card{
 		Secret:   app.config.stripe.secret,
 		Key:      app.config.stripe.key,
@@ -68,3 +79,24 @@ func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request)
 			app.errorLog.Println(err)
 			return
 		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+
+		// But if things don't work as expected.
+	} else {
+		j := jsonResponse{
+			OK:      false,
+			Message: msg,
+			Content: "",
+		}
+
+		out, err := json.MarshalIndent(j, "", "   ")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	}
+}
